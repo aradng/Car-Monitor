83bc78886b8fafa1845835e1a7f9c8c0c6f58a64
aradng <aradarang@gmail.com>
2023-01-09 10:12:19 +0330

    altimiter + manifold gauge + early ADC for O2,afr,battery added

--- bahram-tests/Air-pressure/Air-pressure.ino
+++ bahram-tests/Air-pressure/Air-pressure.ino
@@ -1,15 +1,23 @@
 
-#include <SFE_BMP180.h>
+#include <BMP280_DEV.h>  // Include the BMP280_DEV.h library
 #include <Wire.h>
-#include <vector>
-
 #include "car-lcd.c"
 
 #include <Adafruit_GFX.h>   // Core graphics library
 #include <MCUFRIEND_kbv.h>  // Hardware-specific library
 MCUFRIEND_kbv tft;
 
-SFE_BMP180 pressure;
+BMP280_DEV altimeter(Wire);
+BMP280_DEV manifold(Wire1);
+
+int ecoY = 270;
+const int N_temp = 5, N_pres = 5, N_alt = 5;
+float temperature[N_temp], pressure[N_pres], altitude[N_alt];
+float p_manifold = 0;
+float v_bat = 0;
+float t_water = 0;
+float afr = 0;
+int i_temp = 0, i_pres = 0, i_alt = 0;
 
 #define BLACK 0x0000
 #define RED 0xE022
@@ -18,6 +26,8 @@ SFE_BMP180 pressure;
 #define WHITE 0xFFFF
 #define GREY 0x8410
 
+float calcAverage(float t[], int n);
+void getSensoreData();
 
 void setup() {
   Serial.begin(9600);
@@ -26,26 +36,23 @@ void setup() {
   if (ID == 0xD3D3)
     ID = 0x9481;  // force ID if write-only display
   tft.begin(ID);
-
-  if (pressure.begin())
-    Serial.println("BMP180 init success");
+  if (altimeter.begin(0x76))
+    Serial.println("Altimeter Operational.");
   else {
-
-    Serial.println("BMP180 init fail\n\n");
-    while (1)
-      ;
+    Serial.println("Altimeter init Failed!");
   }
 
+  if (manifold.begin(0x76))
+    Serial.println("Manifold Pressure Sensor Operational.");
+  else {
+    Serial.println("Manifold init Failed!");
+  }
+  altimeter.setTimeStandby(TIME_STANDBY_250MS);  // Set the standby time to 2 seconds
+  altimeter.startNormalConversion();             // Start BMP280 continuous conversion in NORMAL_MODE
+  manifold.setTimeStandby(TIME_STANDBY_250MS);   // Set the standby time to 2 seconds
+  manifold.startNormalConversion();              // Start BMP280 continuous conversion in NORMAL_MODE
   initializeSetup();
 }
-
-int ecoY = 270;
-char status;
-double T, P, p0, p1, a;
-double H = 0;
-std::vector<double> averageHeight;
-std::vector<double> averageTemp;
-
 
 void initializeSetup() {
   tft.setRotation(3);
@@ -59,24 +66,7 @@ void initializeSetup() {
 
   tft.setTextColor(WHITE, BLACK);
 
-  for (int i = 0; i < 5; i++) {
-    getSensoreData();
-    averageHeight.emplace_back(H);
-    averageTemp.emplace_back(T);
-  };
-
   drawEco();
-}
-
-double calcAverage(std::vector<double> const& v) {
-  if (v.empty()) {
-    return 0;
-  }
-  double sum = 0;
-  for (int i = 0; i < v.size(); i++) {
-    sum += v.at(i);
-  }
-  return sum / v.size();
 }
 
 void loop() {
@@ -89,62 +79,44 @@ void loop() {
     drawEco();
   };
 
-  averageHeight.emplace_back(H);
-  averageHeight.erase(averageHeight.begin());
-
-  averageTemp.emplace_back(T);
-  averageTemp.erase(averageTemp.begin());
-
   tft.setTextSize(4);
 
   tft.setCursor(258, 76);
-  tft.print(calcAverage(averageTemp), 1);
+  tft.print(calcAverage(temperature, N_temp), 1);
   tft.print(" C");
 
   tft.setCursor(258, 216);
-  tft.print(calcAverage(averageHeight), 0);
+  tft.print(calcAverage(altitude, N_alt), 0);
   tft.print(" M");
 
   delay(100);
 }
 
 void getSensoreData() {
-  status = pressure.startTemperature();
-  if (status != 0) {
-    delay(status);
-    status = pressure.getTemperature(T);
-    if (status != 0) {
-      Serial.print("temperature: ");
-      Serial.print(T, 2);
-      Serial.println(" deg C, ");
+  float t, p, a;
+  if (altimeter.getMeasurements(t, p, a)) {
+    temperature[i_temp % N_temp] = t;
+    pressure[i_pres % N_pres] = p;
+    altitude[i_alt % N_alt] = a;
+    i_temp++;
+    i_pres++;
+    i_alt++;
+  }
+  manifold.getCurrentPressure(p_manifold);
+  ecoY = map(p_manifold, 300, 1010, 270, 46);
 
-      status = pressure.startPressure(3);
-      if (status != 0) {
-        delay(status);
-        status = pressure.getPressure(P, T);
-        if (status != 0) {
-          Serial.print("absolute pressure: ");
-          Serial.print(P, 2);
-          Serial.println("mba, ");
+  v_bat = analogRead(A11)*5*3.3/1023;
+  Serial.print("battery voltage (V): ");
+  Serial.println(v_bat);
+  afr = analogRead(A8);
+  t_water = analogRead(A9);
+}
 
-          H = ((pow(1013.25 / P, 1 / 5.257) - 1) * (325.15)) / 0.0065;
-
-          // Todo
-          p1 = P;
-          // if (p1 > 850) {
-          //   p1 = 850;
-          // } else if (p1 < 350) {
-          //   p1 = 350;
-          // };
-
-          // ecoY = map(p1, 350, 850, 270, 46);
-
-          ecoY = map(p1, 300, 1010, 270, 46);
-
-        } else Serial.println("error retrieving pressure measurement\n");
-      } else Serial.println("error starting pressure measurement\n");
-    } else Serial.println("error retrieving temperature measurement\n");
-  } else Serial.println("error starting temperature measurement\n");
+float calcAverage(float t[], int n) {
+  double sum = 0;
+  for (int i = 0; i < n; i++)
+    sum += t[i];
+  return sum / n;
 }
 
 void drawEco() {
@@ -157,5 +129,5 @@ void drawEco() {
   }
   tft.setCursor(14, 288);
   tft.setTextSize(3);
-  tft.print(P / 10, 0);
+  tft.print(p_manifold / 10, 0);
 }
