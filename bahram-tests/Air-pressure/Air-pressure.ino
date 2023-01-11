ef9eb4ab663a8b65c7c76583536198ab36e584b5
Bahram Azimbeyk <z.zolfian@gmail.com>
2023-01-11 17:06:21 +0330

    refactor avr method :)

--- bahram-tests/Air-pressure/Air-pressure.ino
+++ bahram-tests/Air-pressure/Air-pressure.ino
@@ -1,6 +1,7 @@
 
 #include <BMP280_DEV.h>  // Include the BMP280_DEV.h library
 #include <Wire.h>
+#include <vector>
 #include "car-lcd.c"
 
 #include <Adafruit_GFX.h>   // Core graphics library
@@ -11,13 +12,14 @@ BMP280_DEV altimeter(Wire);
 BMP280_DEV manifold(Wire1);
 
 int ecoY = 270;
-const int N_temp = 5, N_pres = 5, N_alt = 5;
-float temperature[N_temp], pressure[N_pres], altitude[N_alt];
+float T, P, A;
+std::vector<double> temperature;
+std::vector<double> altitude;
 float p_manifold = 0;
 float v_bat = 0;
 float t_water = 0;
 float afr = 0;
-int i_temp = 0, i_pres = 0, i_alt = 0;
+int i_avr = 0;
 
 #define BLACK 0x0000
 #define RED 0xE022
@@ -26,10 +28,11 @@ int i_temp = 0, i_pres = 0, i_alt = 0;
 #define WHITE 0xFFFF
 #define GREY 0x8410
 
-float calcAverage(float t[], int n);
 void getSensoreData();
 
 void setup() {
+  delay(100);
+
   Serial.begin(9600);
   uint16_t ID = tft.readID();
 
@@ -47,20 +50,25 @@ void setup() {
   else {
     Serial.println("Manifold init Failed!");
   }
-  altimeter.setTimeStandby(TIME_STANDBY_250MS);  // Set the standby time to 2 seconds
-  altimeter.startNormalConversion();             // Start BMP280 continuous conversion in NORMAL_MODE
-  manifold.setTimeStandby(TIME_STANDBY_250MS);   // Set the standby time to 2 seconds
-  manifold.startNormalConversion();              // Start BMP280 continuous conversion in NORMAL_MODE
+  altimeter.setTimeStandby(TIME_STANDBY_05MS);  // Set the standby time to 2 seconds
+  altimeter.startNormalConversion();            // Start BMP280 continuous conversion in NORMAL_MODE
+  manifold.setTimeStandby(TIME_STANDBY_05MS);   // Set the standby time to 2 seconds
+  manifold.startNormalConversion();             // Start BMP280 continuous conversion in NORMAL_MODE
+  for (int j = 0; j < 4; j++) {
+    getSensoreData();
+    altitude.emplace_back(A);
+    temperature.emplace_back(T);
+  }
   initializeSetup();
 }
 
 void initializeSetup() {
   tft.setRotation(3);
   tft.fillScreen(BLACK);
-  tft.drawRGBBitmap(157, 43, vwLogo, 153, 145);
-  tft.drawRGBBitmap(145, 225, name, 189, 15);
+  // tft.drawRGBBitmap(157, 43, vwLogo, 153, 145);
+  // tft.drawRGBBitmap(145, 225, name, 189, 15);
 
-  delay(3000);
+  // delay(1000);
 
   tft.drawRGBBitmap(0, 0, car_lcd, 480, 320);
 
@@ -79,44 +87,62 @@ void loop() {
     drawEco();
   };
 
+  altitude.emplace_back(A);
+  altitude.erase(altitude.begin());
+
+  temperature.emplace_back(T);
+  temperature.erase(temperature.begin());
+
   tft.setTextSize(4);
+  
+  tft.setCursor(78, 76);
+  tft.print(v_bat, 1);
+  tft.print(" V");
 
   tft.setCursor(258, 76);
-  tft.print(calcAverage(temperature, N_temp), 1);
+  tft.print(calcAverage(temperature), 1);
   tft.print(" C");
+  Serial.print(calcAverage(temperature));
+  Serial.println(" avr temp");
 
-  tft.setCursor(258, 216);
-  tft.print(calcAverage(altitude, N_alt), 0);
+  tft.setCursor(258, 226);
+  tft.print(calcAverage(altitude), 0);
   tft.print(" M");
+  Serial.print(calcAverage(altitude));
+  Serial.println(" avr alt");
 
   delay(100);
 }
 
 void getSensoreData() {
-  float t, p, a;
-  if (altimeter.getMeasurements(t, p, a)) {
-    temperature[i_temp % N_temp] = t;
-    pressure[i_pres % N_pres] = p;
-    altitude[i_alt % N_alt] = a;
-    i_temp++;
-    i_pres++;
-    i_alt++;
-  }
+  altimeter.getCurrentMeasurements(T, P, A);
+
   manifold.getCurrentPressure(p_manifold);
-  ecoY = map(p_manifold, 300, 1010, 270, 46);
+  float p1 = p_manifold;
+  if (p1 > 850) {
+    p1 = 850;
+  } else if (p1 < 350) {
+    p1 = 350;
+  };
+  // ecoY = map(p1, 300, 1010, 270, 46);
+  ecoY = map(p1, 350, 850, 270, 46);
 
   v_bat = analogRead(A11)*5*3.3/1023;
-  Serial.print("battery voltage (V): ");
-  Serial.println(v_bat);
-  afr = analogRead(A8);
-  t_water = analogRead(A9);
+  // Serial.print("battery voltage (V): ");
+  // Serial.println(v_bat);
+  // afr = analogRead(A8);
+  // t_water = analogRead(A9);
 }
 
-float calcAverage(float t[], int n) {
+double calcAverage(std::vector<double> const& v) {
+  if (v.empty()) {
+    return 0;
+  }
   double sum = 0;
-  for (int i = 0; i < n; i++)
-    sum += t[i];
-  return sum / n;
+  for (int i = 0; i < v.size(); i++) {
+    sum += v.at(i);
+  }
+  return sum / v.size();
 }
 
 void drawEco() {
